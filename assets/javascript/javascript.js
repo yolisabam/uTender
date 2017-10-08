b65407212e6fb9a6a5b07c88988846b7054f59ac
Winfred Sunga <winfredsunga01@gmail.com>
2017-10-07 19:36:08 -0700

    fixed bug in date format validation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,7 +2,7 @@
 var firstName = "";
 var dob = "";
 var cocktail = "";
-var dobValidationMsg = "DOB is not in proper format!";
+//var dobValidationMsg = "DOB is not in proper format!";
 var age = 0;
 
 
@@ -17,8 +17,8 @@ $("#startAssesment").on("click", function(){
   //grab the dob provided by user
   firstName = $("#name-input").val().trim();
   dob = $("#birth-input").val().trim();
-  dob = moment(dob).format("MM/DD/YYYY");
-  age = moment().diff(dob, "years");
+  dobMoment = moment(dob).format("MM DD YYYY");
+  age = moment().diff(dobMoment, "years");
   
   console.log("firstName : " + firstName);
   console.log("dob : " + dob);
@@ -32,9 +32,9 @@ $("#startAssesment").on("click", function(){
     $("#name-label").append(validation);
   }
   //validate if the dob is in the proper format, the front-end framework will return an empty string as the value if the improper format is provided
-  else if (dob === "") {
+  else if (dob === "" || dob.toLowerCase() === "invalid date" ||dob.split("/")[0] === "" || dob.split("/")[1] === "" || dob.split("/")[2]) {
     //create a new p tag and the validation message
-    var validation = $("<p>").attr("id", "dob-validation-msg").text(dobValidationMsg);
+    var validation = $("<p>").attr("id", "dob-validation-msg").text("DOB is not in proper format!");
     
     //display the validation message  between the dob label and input text box
     $("#dob-label").append(validation);
